207f255179ab2c6def9c30b132ab3337e6f5372b
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2022-10-12 20:54:25 +0300

    New implementation for cli arguments parsing

--- psg-tools/src/psgtools/PSGTools.cpp
+++ psg-tools/src/psgtools/PSGTools.cpp
@@ -3,7 +3,6 @@
 #include "ConvertFiles.h"
 #include "interface/ConsoleGUI.h"
 
-static const std::string c_favoritesPath = "favorites.m3u";
 static Termination m_termination = false;
 
 BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
@@ -32,12 +31,11 @@ void PrintWellcome()
 
 int main(int argc, char* argv[])
 {
-#if 0 // sample.psg test/*.psg --favorites fav.m3u -o psg
     argparse::ArgumentParser program("PSG Tools", "1.0");
 
     program.add_argument("input")
         .help("input file(s) or directory")
-        .default_value(c_favoritesPath)
+        .default_value<std::vector<std::string>>({ "favorites.m3u" })
         .nargs(argparse::nargs_pattern::any);
 
     program.add_argument("-o", "--output")
@@ -57,7 +55,7 @@ int main(int argc, char* argv[])
     program.add_argument("--favorites")
         .help("user defined favorites playlist")
         .metavar("PATH")
-        .default_value(c_favoritesPath);
+        .default_value(std::string{ "favorites.m3u" });
 
     try { program.parse_args(argc, argv); }
     catch (const std::runtime_error& err) 
@@ -68,8 +66,8 @@ int main(int argc, char* argv[])
     }
 
     // parse list of input files
-    auto inputs = program.get<std::vector<std::string>>("input");
     Filelist filelist(PSGHandler::DecodeFileTypes);
+    auto inputs{ program.get<std::vector<std::string>>("input") };
     for (const auto& path : inputs) filelist.Append(path);
 
     // TODO: parse output file or output type
@@ -131,71 +129,5 @@ int main(int argc, char* argv[])
             psgPlayer.Play();
         }
     }
-
-#else
-    bool isConverting = false;
-    bool isRandomShuffle = false;
-    bool isStreamer = false;
-
-    std::filesystem::path inputPath;
-    std::filesystem::path outputPath;
-
-    // setup input path
-    if (argc > 1)
-        inputPath = argv[1];
-    else
-        inputPath = c_favoritesPath;
-    
-    // setup output path
-    if (argc > 2)
-    {
-        outputPath = argv[2];
-        isConverting = true;
-    }
-
-    // setup file lists
-    Filelist filelist(PSGHandler::DecodeFileTypes, inputPath);
-    Filelist favorites(PSGHandler::DecodeFileTypes, c_favoritesPath);
-
-    // setup destination chip config
-    Chip chip;
-#if 0
-    chip.first.model(Chip::Model::AY8930);
-    chip.second.model(Chip::Model::AY8910);
-    chip.output(Chip::Output::Stereo);
-    chip.stereo(Chip::Stereo::CAB);
-    chip.clockValue(1000000);
-#endif
-
-    PrintWellcome();
-    SetConsoleCtrlHandler(CtrlHandler, TRUE);
-
-    if (!filelist.IsEmpty())
-    {
-        if (isConverting)
-        {
-            //ConvertInputFiles(outputPath);
-        }
-        else
-        {
-            // setup output device
-            std::shared_ptr<Output> output;
-            if (isStreamer)
-            {
-                const int k_comPortIndex = 4;
-                output.reset(new Streamer(k_comPortIndex));
-            }
-            else
-            {
-                output.reset(new Emulator());
-            }
-
-            // setup and start playback
-            if (isRandomShuffle) filelist.RandomShuffle();
-            PSGPlayer psgPlayer(chip, *output, filelist, favorites, m_termination);
-            psgPlayer.Play();
-        }
-    }
-#endif
     return 0;
 }
