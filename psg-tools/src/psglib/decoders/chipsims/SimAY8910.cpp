8c1d95203bcbf6f9e4d8064ff0d553110da1dc3c
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2022-10-16 17:22:10 +0300

    Added clock rate conversion to AY8910 simulator for VGM decoder

--- psg-tools/src/psglib/decoders/chipsims/SimAY8910.cpp
+++ psg-tools/src/psglib/decoders/chipsims/SimAY8910.cpp
@@ -10,6 +10,9 @@ void SimAY8910::Reset()
 {
 	m_frame.ResetData();
 	m_frame.ResetChanges();
+
+    m_pframe.ResetData();
+    m_pframe.ResetChanges();
 }
 
 void SimAY8910::Write(int chip, Register reg, uint8_t data)
@@ -24,6 +27,36 @@ void SimAY8910::Simulate(int samples)
 
 void SimAY8910::Convert(Frame& frame)
 {
-	frame = m_frame;
-	m_frame.ResetChanges();
+    m_pframe += m_frame;
+    m_frame.ResetChanges();
+
+    static const PeriodRegister c_com[]
+    {
+        A_Period, B_Period, C_Period,
+        E_Period, N_Period
+    };
+
+    static const PeriodRegister c_exp[]
+    {
+        A_Period,  B_Period,  C_Period,
+        EA_Period, EB_Period, EC_Period,
+        N_Period
+    };
+
+    if (m_clockRatio != 1.f)
+    {
+        for (int chip = 0; chip < 2; ++chip)
+        {
+            bool isExpMode = m_pframe[chip].IsExpMode();
+            for (size_t i = 0; i < (isExpMode ? sizeof(c_exp) : sizeof(c_com)); ++i)
+            {
+                PeriodRegister preg = (isExpMode ? c_exp[i] : c_com[i]);
+                uint16_t period = ConvertPeriod(m_pframe[chip].ReadPeriod(preg));
+                m_pframe[chip].UpdatePeriod(preg, period);
+            }
+        }
+    }
+
+    frame += m_pframe;
+    m_pframe.ResetChanges();
 }
--- psg-tools/src/psglib/decoders/chipsims/SimAY8910.h
+++ psg-tools/src/psglib/decoders/chipsims/SimAY8910.h
@@ -14,4 +14,5 @@ public:
 
 private:
 	Frame m_frame;
+	Frame m_pframe;
 };
