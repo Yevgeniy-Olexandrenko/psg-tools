711fa4d413c760e8c800f158200b4476f09e0406
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2023-12-14 21:00:40 +0200

    Clean up project

--- psg-tools/src/psglib/output/Output.h
+++ psg-tools/src/psglib/output/Output.h
@@ -6,10 +6,7 @@
 
 #define DBG_PROCESSING 0
 
-//#define DEBUG_TEST
-#if defined(DEBUG_TEST)
-#include "../../debug/psg-access.h"
-#endif
+#define DEBUG_TEST
 
 class Stream;
 class Frame;
@@ -53,11 +50,4 @@ private:
 	Chip m_dchip;
 	bool m_isOpened;
 	ProcChain m_procChain;
-
-#if defined(DEBUG_TEST)
-	PSG m_psg;
-	Data m_output;
-	uint8_t m_reg = 0xFF;
-	void debug_psg_write(uint8_t data);
-#endif
 };
