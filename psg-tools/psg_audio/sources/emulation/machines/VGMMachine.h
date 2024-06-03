e7d4b121f08713340663793a25d4297738792187
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2024-06-03 20:28:41 +0300

    Update VGMMachine.h

--- psg-tools/psg_audio/sources/emulation/machines/VGMMachine.h
+++ psg-tools/psg_audio/sources/emulation/machines/VGMMachine.h
@@ -1,6 +1,34 @@
 #pragma once
 
-class VGMMachine
+#include "emulation/Machine.h"
+#include "stream/Frame.h"
+
+class VGMMachine : public Machine
+{
+public:
+	virtual void Init() = 0;
+	virtual void Write(int chip, addr_t reg, data_t data) = 0;
+	virtual void DataBlock(addr_t addr, size_t size) = 0;
+	virtual void Simulate(int samples) = 0;
+	virtual void Convert(Frame& frame) = 0;
+};
+
+class AY89XXPsgMachine : public VGMMachine
+{
+
+};
+
+class RP2A03ApuMachine : public VGMMachine
+{
+
+};
+
+class SN76489PsgMachine : public VGMMachine
+{
+
+};
+
+class GBDMGApuMachine : public VGMMachine
 {
 
 };
